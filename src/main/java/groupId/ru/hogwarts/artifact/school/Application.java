15cb162340161c47db1fc8aad53e4e8c43d77f0f
serge <sergei.g.d@gmail.ccom>
2022-09-30 20:03:29 +0700

    1

---
+++ src/main/java/groupId/ru/hogwarts/artifact/school/Application.java
@@ -0,0 +1,13 @@
+package groupId.ru.hogwarts.artifact.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
